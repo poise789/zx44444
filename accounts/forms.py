e2974faa2a1db4c2ac9a4c12f9ce93080830fb8c
poise789 <1395497632@qq.com>
2024-11-08 09:55:32 +0800

    第一次提交

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class AuthenticateForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs) :
+        super(AuthenticateForm, self).__init__(*args, **kwargs)
+        for fieldname in ['username', 'password']:
+            self.fields[fieldname].help_text = None
+            self.fields[fieldname].widget.attrs.update({'class':'form-control'})
+
+
+
+class UserCreateForm(UserCreationForm) :
+    def __init__(self, *args, **kwargs) :
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].widget.attrs.update({'class':'form-control'})
